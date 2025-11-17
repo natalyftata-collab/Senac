40ba547b56d392094d452bafc94e38b0d8cbc9ce
natalyftata-collab <natalyftata@gmail.com>
2025-11-17 15:44:13 -0300

    primeiro commit

---
+++ public/js/booking.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async(e)=>{
+    e.preventDefault();
+
+    const nome = document.querySelector("#name").value;
+    const telefone = document.querySelector("#phone").value;
+    const servico = document.querySelector("#service").value;
+    const data = document.querySelector("#date").value;
+    const horario = document.querySelector("#time").value;
+
+    try {
+        const resposta = await fetch("http://localhost:3000/agendamentos",{
+            method:"POST",
+            headers:{"Content-Type": "application/json"},
+            body: JSON.stringify(
+            {
+                nome,
+                telefone,
+                servico,
+                data,
+                horario
+            })
+        })
+
+        const resultado = await resposta.text();
+        alert(resultado);
+
+    } catch (error) {
+        alert("Erro ao enviar agendamento" + error)
+    }
+});
+
